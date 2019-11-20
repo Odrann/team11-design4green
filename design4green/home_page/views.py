29d872258b434d6f85d770776330a481db5ffdf0
Richard <44785542+LAROZE-Richard@users.noreply.github.com>
2019-11-20 15:34:36 +0100

    edit test page

--- design4green/home_page/views.py
+++ design4green/home_page/views.py
@@ -8,7 +8,4 @@ def index(request):
     return HttpResponse("<h1> Test2 </h1>")
 
 def newtest(request):
-    template = loader.get_template('templates/index.html')
-    context = {
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'index/index.html')
