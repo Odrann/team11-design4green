5f22b6f5482bfabaf3661dde8baec07995f21146
Kdudek <45154839+kdudekepita@users.noreply.github.com>
2019-11-20 13:23:20 +0100

    Update views.py

--- design4green/home_page/views.py
+++ design4green/home_page/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from .models import Project
 from django.http import Http404
-# VIEWS home_page.
 
 def index(request):
     return render(request, "index/index.html")
