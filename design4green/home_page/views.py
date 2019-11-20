65a6850d69cd1d7ef36ddb099e151e5d3b01f7a6
kdudekepita <45154839+kdudekepita@users.noreply.github.com>
2019-11-20 22:43:43 +0100

    update

--- design4green/home_page/views.py
+++ design4green/home_page/views.py
@@ -14,7 +14,7 @@ def user_details(request, user_id):
     # try catch on one line
     user = get_object_or_404(Utilisateur, pk=user_id)
 
-    return render(request, 'index/user_details.html', {'user': user})
+    return render(request, 'details/user_details.html', {'user': user})
 
 
 
