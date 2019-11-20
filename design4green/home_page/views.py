3773470f1ec74d46cab2094fd82366629bf8af25
Richard <44785542+LAROZE-Richard@users.noreply.github.com>
2019-11-21 00:30:09 +0100

    change conso

--- design4green/home_page/views.py
+++ design4green/home_page/views.py
@@ -4,9 +4,6 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.http import HttpResponse
 
-from io import StringIO
-import cairo
-
 # MODEMS IMPORTS
 from .models import Utilisateur
 
@@ -25,35 +22,4 @@ def user_details(request, user_id):
 
 
 def conso(request):
-    svg_buffer = StringIO()
-
-    width, height = (500, 400)
-    surface = cairo.SVGSurface(svg_buffer, width, height)
-
-    dataSet = (
-        ('dataSet 1', ((0, 1), (1, 3), (2, 2.5))),
-        ('dataSet 2', ((0, 2), (1, 4), (2, 3))),
-        ('dataSet 3', ((0, 5), (1, 1), (2, 0.5))),
-    )
-
-    options = {
-        'legend': {'hide': True},
-        'background': {'color': '#f0f0f0'},
-    }
-
-    # import pycha.bar
-    # chart = pycha.bar.VerticalBarChart(surface, options)
-
-    import pycha.line
-    chart = pycha.line.LineChart(surface, options)
-    chart.addDataset(dataSet)
-    chart.render()
-
-    del chart
-    del surface
-
-    response = ''
-    response = HttpResponse(mimetype='image/svg+xml')
-    svg_buffer.seek(0)
-    response.write(svg_buffer.read())
-    return response
+    return render(request, 'index/consommation.html')
