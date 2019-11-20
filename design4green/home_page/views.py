1efd049f029def0869fe20a116985e59ec89eb35
Richard <44785542+LAROZE-Richard@users.noreply.github.com>
2019-11-20 15:21:10 +0100

    Add Test call

--- design4green/home_page/views.py
+++ design4green/home_page/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1> Test2 </h1>")
+
+def newtest(request):
+    template = loader.get_template('templates/index.html')
+    context = {
+    }
+    return HttpResponse(template.render(context, request))
