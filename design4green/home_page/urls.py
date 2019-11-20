0d938a9ebf5ca41415e50f6850bf6645a80ed7d5
Richard <44785542+LAROZE-Richard@users.noreply.github.com>
2019-11-20 18:08:29 +0100

    add conso begin

--- design4green/home_page/urls.py
+++ design4green/home_page/urls.py
@@ -10,5 +10,5 @@ app_name = "homePage"
 urlpatterns = [
         #index for the home page
         url(r'^$', views.index, name='index'),
-        url(r'^test/$', views.newtest, name='newtest'),
+        url(r'^consommation/$', views.conso, name='conso'),
     ]
--- design4green/home_page/views.py
+++ design4green/home_page/views.py
@@ -7,5 +7,36 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("<h1> Test2 </h1>")
 
-def newtest(request):
-    return render(request, 'index/index.html')
+def conso(request):
+    svg_buffer = StringIO()
+
+    width, height = (500, 400)
+    surface = cairo.SVGSurface(svg_buffer, width, height)
+
+    dataSet = (
+        ('dataSet 1', ((0, 1), (1, 3), (2, 2.5))),
+        ('dataSet 2', ((0, 2), (1, 4), (2, 3))),
+        ('dataSet 3', ((0, 5), (1, 1), (2, 0.5))),
+    )
+
+    options = {
+        'legend': {'hide': True},
+        'background': {'color': '#f0f0f0'},
+    }
+
+    # import pycha.bar
+    # chart = pycha.bar.VerticalBarChart(surface, options)
+
+    import pycha.line
+    chart = pycha.line.LineChart(surface, options)
+    chart.addDataset(dataSet)
+    chart.render()
+
+    del chart
+    del surface
+
+    response = ''
+    response = HttpResponse(mimetype='image/svg+xml')
+    svg_buffer.seek(0)
+    response.write(svg_buffer.read())
+    return response
