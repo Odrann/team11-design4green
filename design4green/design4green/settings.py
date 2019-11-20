06fb56687b6c4cc59dbb56378e4ffe233fc5b99d
root <root@vps753512.ovh.net>
2019-11-20 12:30:36 +0100

    Creation of app home page

--- design4green/design4green/settings.py
+++ design4green/design4green/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e_7v*bzxfzy!w@6pfw0f7)lf8_6*8zzmt@h%*%engd_dxun72k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
