65a89dc1c438264345beb3ccb4f68b188b38f7ee
Richard <44785542+LAROZE-Richard@users.noreply.github.com>
2019-11-21 16:23:13 +0100

    a

--- design4green/home_page/static/script/recovery/recovery.js
+++ design4green/home_page/static/script/recovery/recovery.js
@@ -2,11 +2,11 @@ $( document ).ready(function() {
 	$('#mail').on("change keyup paste click", function(){
 		if ($('#mail').val().length > 3)
 		{
-			$('.btn_envoyer').removeAttr('disabled');
+			$('#btn_envoyer').removeAttr('disabled');
 		}
 		else
 		{
-			$('.btn_envoyer').attr('disabled', 'disabled');
+			$('#btn_envoyer').attr('disabled', 'disabled');
 		}
 	});
 });
