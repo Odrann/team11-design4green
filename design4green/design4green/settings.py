dbc6063713fd2e7edb33b774511bfa3298eb6129
Richard <44785542+LAROZE-Richard@users.noreply.github.com>
2019-11-21 18:50:15 +0100

    test gmail

--- design4green/design4green/settings.py
+++ design4green/design4green/settings.py
@@ -128,9 +128,9 @@ STATIC_URL = '/static/'
 
 DATE_INPUT_FORMATS = ['%d-%m-%Y']
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = 465
-EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
