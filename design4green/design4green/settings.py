6217997bbda1bdcf6a1bb6c62a63eaae64bf1d43
Richard <44785542+LAROZE-Richard@users.noreply.github.com>
2019-11-21 19:07:03 +0100

    tst mail

--- design4green/design4green/settings.py
+++ design4green/design4green/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 
 DATE_INPUT_FORMATS = ['%d-%m-%Y']
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
