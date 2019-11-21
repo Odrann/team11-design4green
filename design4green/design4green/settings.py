117d7063e1d559fec03513e1251f92b589c0f73e
Richard <44785542+LAROZE-Richard@users.noreply.github.com>
2019-11-21 18:52:17 +0100

    test gmail mail

--- design4green/design4green/settings.py
+++ design4green/design4green/settings.py
@@ -132,5 +132,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'design4green.test@gmail.com'
+EMAIL_HOST_PASSWORD = 'ruyfgu4845258;'
