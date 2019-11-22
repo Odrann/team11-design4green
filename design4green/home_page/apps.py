61876b0640b2736cb215b22beb250e500666e2d3
Kdudek <45154839+kdudekepita@users.noreply.github.com>
2019-11-22 05:26:01 +0100

    Update apps.py

--- design4green/home_page/apps.py
+++ design4green/home_page/apps.py
@@ -1,14 +1,11 @@
 # HOME PAGE APPS
 from django.apps import AppConfig
 from django.contrib import admin
-from .models import Habitant, Utilisateur, Logement, Consommation, Proprietaire
+from .models import Habitant
 
 
 admin.site.register(Habitant)
-admin.site.register(Utilisateur)
-admin.site.register(Logement)
-admin.site.register(Consommation)
-admin.site.register(Proprietaire)
+
 
 class HomePageConfig(AppConfig):
     name = 'home_page'
