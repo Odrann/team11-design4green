e4bd1fa027b65b7ad33d8c4bc76dc8f1f1a6d3dc
Kdudek <45154839+kdudekepita@users.noreply.github.com>
2019-11-22 05:01:36 +0100

    qsf

--- design4green/home_page/apps.py
+++ design4green/home_page/apps.py
@@ -1,6 +1,8 @@
 # HOME PAGE APPS
-
 from django.apps import AppConfig
+from django.contrib import admin
+from .models import * as allmod
 
+admin.site.register(allmod)
 class HomePageConfig(AppConfig):
     name = 'home_page'
