e0b447851d19d4d0b01b1575048237d418f5a9ef
Richard <44785542+LAROZE-Richard@users.noreply.github.com>
2019-11-22 05:57:58 +0100

    az

--- design4green/home_page/views.py
+++ design4green/home_page/views.py
@@ -22,8 +22,8 @@ def index(request):
     mdp = request.POST.get('mdp')
 
     if (user != '' and user is not None) or (mdp != '' and mdp is not None):
-        searchuser = Utilisateur.objects.get(u_id=user)
-        searchpass = searchuser.objects.get(u_mdp=mdp)
+        searchuser = get_object_or_404(Utilisateur, u_id=user)
+        searchpass = searchuser.objects.get(Utilisateur, u_mdp=mdp)
 
         if searchuser != '' and searchuser is not None and searchpass != '' and searchpass is not None:
             return render(request, "details/user_details.html", searchuser.id)
